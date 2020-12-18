b148396f762eac4e573a3db483bb337986efa5bd
heshamamohamed <heshamamohamed323@gmail.com>
2020-12-18 07:40:41 +0200

    implemented phase 1, 2 players 1 round

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,7 +10,7 @@ describe Game do
     describe "#initialize" do
         let(:testGame) {Game.new(player1, player2)}
 
-        it "has an dictionary" do
+        it "has a dictionary" do
             expect(testGame.dictionary).to_not be nil
         end
 
@@ -25,59 +25,140 @@ describe Game do
     end
 
 
-    describe "#play_round" do
-        
-        describe "#current_player" do
-            it "returns the current player"
+    describe "#play" do
 
-        end
+        describe "#play_round" do
 
-        describe "#previous_player" do
-            it "returns the previous player"
+            describe "#not_dead_end?" do
+                
+                it "returns true if the new fragment is a starting of an existing word" do
+                    words = game.dictionary
+                    game.fragment = "sug"
+                    expect(game.not_dead_end?("a")).to be true
+                end
+                it "returns false if the new fragment is a starting of an non-existing word" do
+                    words = game.dictionary
+                    game.fragment = "sug"
+                    expect(game.not_dead_end?("x")).to be false
+                end
+            end
 
-        end
+            describe "#is_a_letter?" do
+                it "returns true if it is a letter" do
+                    expect(game.is_a_letter?('a')).to be true
+                    expect(game.is_a_letter?('d')).to be true
+                end
 
-        describe "#next_player!" do
-            it "updates the current_player and the previous_player"
+                it "returns false if it is not a letter" do
+                    expect(game.is_a_letter?('1')).to be false
+                    expect(game.is_a_letter?('!')).to be false
+                end
+            end
 
-        end
+            describe "#valid_play?" do
+                it "Checks that string is a letter of the alphabet" do
+                    expect(game.valid_play?('a')).to be true
+                    expect(game.valid_play?('d')).to be true
 
-        describe "#valid_play?" do
-            it "Checks that string is a letter of the alphabet"
+                    expect(game.valid_play?(100)).to be false
+                    expect(game.valid_play?('!')).to be false
+                end
 
-            it "Checks if there are words we can spell after adding it to the fragment"
+                it "Checks if there are words we can spell after adding it to the fragment" do
+                    game.fragment = "sug"
+                    expect(game.valid_play?('a')).to be true
+                    game.fragment = "sug"
+                    expect(game.valid_play?('x')).to be false
+                end
+            end
 
-        end
+            describe "#current_player" do
+                it "returns the current player" do
+                    expect(game.current_player.name).to eq("p1")
+                end
+            end
 
-        describe "#add_letter_to_fragment" do
-            it "adds a letter to the fragment"
+            describe "#previous_player" do
+                it "returns the previous player" do
+                    expect(game.previous_player).to be nil
+                    game.next_player!
+                    expect(game.previous_player.name).to eq("p1")
+                    game.next_player!
+                    expect(game.previous_player.name).to eq("p2")
+                end
 
-        end
+            end
 
-        describe "#take_turn" do
-            it "takes a string from the player untill a valid play is made"
+            describe "#next_player!" do
+                it "updates the current_player and the previous_player" do
+                    expect(game.current_player.name).to eq(player1.name)
+                    expect(game.previous_player).to be nil
 
-            it "updates the fragment and check against dictionary"
+                    game.next_player!
 
-            it "alerts player if they make an invalid move."
+                    expect(game.current_player.name).to eq(player2.name)
+                    expect(game.previous_player.name).to eq(player1.name)
+
+                end
+
+            end
             
+            describe "#update_fragment" do
+                it "adds a letter to the fragment" do
+                    game.update_fragment('a')
+                    expect(game.fragment).to eq('a')
+                    game.update_fragment('b')
+                    expect(game.fragment).to eq('ab')
+                    game.update_fragment('c')
+                    expect(game.fragment).to eq('abc')
+                end
+
+            end
+
+            describe "#show_letter_and_fragment" do
+                it "shows entered letter and current fragment" do
+                    game.fragment = "do"
+                    expect(game.show_letter_and_fragment). to include("letter entered: o")
+                end
+            end
+
+            describe "#take_turn" do
+
+                it "takes a letter, updates the fragment, then rotate the players." do
+                    expect(game.fragment).to eq("")
+                    expect(game.current_player.name).to eq(game.players[0].name)
+                    
+                    game.take_turn(game.current_player, "a")
+                    
+                    expect(game.fragment).to eq("a")
+                    expect(game.current_player.name).to eq(game.players[1].name)
+                    
+                    game.take_turn(game.current_player, "p")
+                    
+                    expect(game.fragment).to eq("ap")
+                    expect(game.current_player.name).to eq(game.players[0].name)
+                end
+                
+            end
+            
+            describe "#is_word?" do
+                it "checks if the current fragment is a word" do
+                    expect(game.is_word?("dog")).to be true
+                    expect(game.is_word?("dogggooo")).to be false
+                end
+            end
+            
+            describe "#round_over?" do
+                it "checks if the round is over" do
+                    game.fragment = "dog"
+                    expect(game.round_over?).to be true
+                    game.fragment = "not_a_word"
+                    expect(game.round_over?).to be false
+                end
+            end
+
         end
-
-
-
-
     end
 
-    
-    
-    describe "#add_letter" do
-
-        it "can add a letter to the fragment"
-
-        it "add letter only if the play is valid."
-
-
-
-    end
 
 end
